9277db4ffb8f969d3cdd3d0523332347dd05fb39
Brett Milash <brett.milash@utah.edu>
2018-10-15 14:20:52 -0600

    Jupyter notebooks and related files for the Center for High Performance Computing's python lecture series.

--- notebooks/dogs.py
+++ notebooks/dogs.py
@@ -1,20 +1,28 @@
 class Dog:
+    "Class Dog defines a generic dog."
     def __init__(self,name):
         self.name=name
     def speak(self):
         return "Arf!"
 
 class Poodle(Dog):
+    """Class Poodle defines a special kind of dog, and is derived from
+    the Dog class."""
     def speak(self):
         return "Yip!" 
 
+# This code gets run only if this module is executed, not if it is imported:
 if __name__ == "__main__":
+    # Create a set.
     s=set()
+    # Add some dogs to the set.
     s.add(Dog("Rex"))
     s.add(Poodle("Fifi"))
     s.add(Dog("Spot"))
     s.add(Poodle("Spike"))
+    # For each dog ...
     for dog in s:
+    	# ... print the dogs name and what it says.
         print(dog.name,"says",dog.speak())
 
 print(__name__)
--- square_root.f90
+++
@@ -1,9 +0,0 @@
-! Fortran example
-program main
-
-    implicit none
-    double precision :: x = 17.0
-
-    write(*,'(A,F8.4,A,F12.6)') "The square root of ", x, " is ", sqrt(x)
-
-end program     
