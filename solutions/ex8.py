eebc8c13dbd6c2795fcc8a38ae0a85c8dfccdcd1
Brett Milash <brett.milash@utah.edu>
2022-03-03 09:57:58 -0700

    Wim updated numpy lectures 7, 8, and 9.

--- solutions/ex8.py
+++ solutions/ex8.py
@@ -1,53 +1,25 @@
-import math
 import numpy as np
 import numpy.linalg as la
-import numpy.random as rnd
+import scipy.linalg as sla
 
 print(f"Exercise 8::")
-print(f"  8.1:")
+A = np.array([[3, 1, 1],
+              [2, 4, 2],
+              [1, 1, 3]])
 
-A = rnd.random((6,6))
-B = ( A+A.T)/2.0
+# Step 1:Eigenvalues, eigenvectors
+w ,v = la.eig(A)
+print(f"  Eigenvalues:{w}")
+print(f"  Eigenvectors:\n{v}")
 
-eigval, eigvec = la.eigh(A)
-print(f"  B:\n{B}\n".format(B))
-print(f"    eig. values:\n{eigval}\n")
-print(f"    eig. vector:\n{eigvec}\n")
+# Step 2: Calc. the exponential of the diagonal
+expD = np.diag(np.exp(w))
 
-print(f"  8.2:")
-# Generate the elements randomly
-n = rnd.randint(4,9)
-V = np.arange(1,n+1)
-V = V.astype(dtype='int64')
+# Step 3: Perform a singularity transform 
+res = np.dot(np.dot(v,expD), la.inv(v))   # Same as: res=v @ expD @ la.inv(v)
 
-def genVDM(V):
-    """
-    Generate a Vandermonde matrix based on a vector V
-    V = (x1,x2,....xn)
-    """
-    N  = V.shape[0]
-    A = np.hstack([V[:,np.newaxis]**i for i in range(N)])
-    return A
+print(f"  Matrix exponential of A:\n{res}")
 
-def detVDM(V):
-    N = V.shape[0]
-    res = V - V[0:,np.newaxis]
-    mask = np.fromfunction(lambda i,j: i<j,(N,N))
-    return np.prod(res[mask])
-
-print(f"  N:{n}")
-print(f"  V:\n{V}\n")
-
-A = genVDM(V) 
-detA = detVDM(V)
-print(f"Own Functions::")
-print(f"  A:\n{A}")
-print(f"  det(A):{detA}\n")
-
-B = np.vander(V,increasing=True)
-detB = la.det(B)
-print(f"Check using NumPy::")
-print(f"  A:\n{B}".format(B))
-print(f"  det(A):{detB}\n".format(detB))
-print(f"Diff:{math.fabs(detB-detA):16.12f}")
+# Check: using scipy.linalg
+print(f"  Matrix exponential of A (using scipy):\n{sla.expm(A)}")      
 
